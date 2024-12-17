69c5e0a6ff20a4a9c38df5e928327baa707456c3
Nora <148393173+NoriKeo@users.noreply.github.com>
2024-12-17 17:23:46 +0100

    more,more,more and more test classes

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,70 +1,32 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import java.lang.reflect.Field;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlayerTest {
 
 
     private Player player;
-
-    @Mock
-    private Board mockiBoard;
-
-    @Mock
-    private RowFromBoard mockiRowFromBoard;
-
-    @Mock
-    private Field mockiField;
+    private Board board;
 
     @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        player = new Player();
+    void setUp() {
+        player = Player.getInstance();
+        board = new Board();
+        for (int i = 1; i <= 9; i++) {
+            board.getField(new Position(i)).isEmpty();
+        }
     }
 
     @Test
-    public void testAskInput() {
-        String input = "1";
-
-        System.setIn(new java.io.ByteArrayInputStream(input.getBytes()));
-
-        int resulti = player.askInput(mockiBoard);
-        assertEquals(1, resulti);
+    public void inputValideTest() {
+        String input = "5\n";
+        InputStream stream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(stream);
+        int input2 = player.askInput(board);
+        assertEquals(input2, 5);
     }
-
-    @Test
-    public void testValid() {
-
-        assertTrue(player.isvalid(1));
-        assertFalse(player.isvalid(-1));
-    }
-
-
-   /*@Test
-    public void testFreeField() throws Exception {
-
-       List<RowFromBoard> row = new ArrayList<>();
-       row.add(mockiRowFromBoard);
-
-       Field[] fields = {mockiField, mockiField, mockiField};
-
-       when(mockiBoard.getRows()).thenReturn(row);
-
-       for (Field field : fields) {
-       when(mockiRowFromBoard.getFields()).thenReturn(field);
-       }
-
-       when(mockiField.isEmpty()).thenReturn(false,true,false);
-
-       when(mockiField.getPosition()).thenReturn(new Position(2));
-
-       assertFalse(player.freefield(mockiBoard,1));
-
-       assertTrue(player.freefield(mockiBoard,2));
-   }*/
 }
